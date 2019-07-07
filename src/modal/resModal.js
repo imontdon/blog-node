9124f3c0d396b295370241c6a9d840cb88efe3db
YFZX-DWS-1855 <dongwenshi@zhuojianchina.com>
2019-07-07 11:44:00 +0800

    fixed: redefined modal type

--- src/modal/resModal.js
+++ src/modal/resModal.js
@@ -1,28 +1,28 @@
 class BaseModal {
   constructor(data, message) {
     if (typeof data === 'string') {
-      this.message = data
+      this.ret_info = data
       data = null
       message = null
     }
     if (data) {
-      this.data = data
+      this.ret_data = data
     }
     if (message) {
-      this.message = message
+      this.ret_info = message
     }
   }
 }
 class SuccessModal extends BaseModal {
   constructor(data, message) {
     super(data, message)
-    this.errNum = 0
+    this.ret_code = 0
   }
 }
 class ErrorModal extends BaseModal {
   constructor(data, message) {
     super(data, message)
-    this.errNum = -1
+    this.ret_code = -1
   }
 }
 module.exports = {
